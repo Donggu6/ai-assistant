5f0a10f9b9c9fa034b5438190ecd91d29ed40264
이동건 <dlehdrjs0101@gmail.com>
2025-12-09 23:41:28 +0900

    Test 준비

--- src/main/java/com/deongeon/ai/assistant/AiAssistantApplication.java
+++ src/main/java/com/deongeon/ai/assistant/AiAssistantApplication.java
@@ -3,7 +3,7 @@ package com.deongeon.ai.assistant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.deongeon.ai")
 public class AiAssistantApplication {
 	
 	public static void main(String[] args) {
