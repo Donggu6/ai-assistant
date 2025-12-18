5333806d59793b2885f9325cdf5d9c7061dccc6b
이동건 <dlehdrjs0101@gmail.com>
2025-12-18 21:04:48 +0900

    kk

--- src/main/java/com/deongeon/ai/config/SecurityConfig.java
+++ src/main/java/com/deongeon/ai/config/SecurityConfig.java
@@ -38,6 +38,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();f
+        return new BCryptPasswordEncoder();
     }
 }
